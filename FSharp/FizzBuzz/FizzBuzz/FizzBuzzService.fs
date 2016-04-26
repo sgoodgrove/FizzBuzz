c395f574b86f709718a724530259ee24ef4961c3
stevego <steve.goodgrove@doncaster.gov.uk>
2016-04-26 14:25:39 +0100

    Added FSharp version of FizzBuzz

---
+++ FSharp/FizzBuzz/FizzBuzz/FizzBuzzService.fs
@@ -0,0 +1,12 @@
+﻿namespace FizzBuzz
+
+type public FizzBuzzService () =
+    member public this.FizzBuzzNum n =
+        let div3 = n % 3 = 0
+        let div5 = n % 5 = 0
+
+        match n with
+            | _ when div3 && div5 -> "FizzBuzz"
+            | _ when div3 -> "Fizz"
+            | _ when div5 -> "Buzz"
+            | x -> x.ToString ()
---
+++ FSharp/FizzBuzz/FizzBuzzTests/Tests.fs
@@ -0,0 +1,34 @@
+﻿
+namespace FizzBuzzTests
+
+open FizzBuzz
+open FsUnit
+open NUnit.Framework
+
+[<TestFixture>]
+type FizzBuzzTests () =
+    let fizzer = new FizzBuzzService ()
+
+    [<TestCase(1, "1")>]
+    [<TestCase(2, "2")>]
+    [<TestCase(4, "4")>]
+    member this.``Should return number as string`` (n : int, str : string) =
+        fizzer.FizzBuzzNum n |> should equal str
+
+    [<TestCase(5)>]
+    [<TestCase(10)>]
+    [<TestCase(20)>]
+    member this.``Should return Buzz`` (n) =
+        fizzer.FizzBuzzNum n |> should equal "Buzz"
+
+    [<TestCase(3)>]
+    [<TestCase(6)>]
+    [<TestCase(24)>]
+    member this.``Should return Fizz`` (n) =
+        fizzer.FizzBuzzNum n |> should equal "Fizz"
+
+    [<TestCase(15)>]
+    [<TestCase(30)>]
+    [<TestCase(60)>]
+    member this.``Should return FizzBuzz`` (n) =
+        fizzer.FizzBuzzNum n |> should equal "FizzBuzz"
